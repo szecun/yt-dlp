686d4ddc0d0fcf227d2cb5f5b0b36ac2eba32abe
szecun <38903999+szecun@users.noreply.github.com>
2026-04-28 15:28:34 +0930

    Revert "Update yt-dlp.cmd"
    This reverts commit a18205d1f5c5b717f986f73ad98af439f56ea932.

--- yt-dlp.cmd
+++ yt-dlp.cmd
@@ -1,2 +1,1 @@
 @py -Werror -Xdev "%~dp0yt_dlp\__main__.py" %*
-yt-dlp https://video.squarespace-cdn.com/content/v1/61da6fbf71c3cc206bf40043/2e2e4ba6-b31e-4676-9503-c02cde351c22/segments/mpegts-h264-1920:1080.m3u8?Expires=1777398053&Signature=NTU2YjRhZGVmY2M3ZmQ2Yjk3ODVkZTVmNDA5YjRjNmFmZGMzNmE0NTAzMWMwNDA0MzI3MjY1MjEzOTU2MTBhZQ
